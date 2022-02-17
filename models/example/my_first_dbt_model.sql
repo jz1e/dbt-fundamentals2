20318229276425deb1ebb490fea44438474eea50
jz1e@andrew.cmu.edu <jz1e@andrew.cmu.edu>
2022-02-17 00:02:18 +0000

    dd

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -4,11 +4,11 @@
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
-    Try changing "table" to "view" below
+    Try changing "table" to "view" below    
 */
 
 {{ config(materialized='table') }}
-
+  
 with source_data as (
 
     select 1 as id
